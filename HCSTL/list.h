67b22b10e5f4cd4b4947e3875271cda475670fca
HuangCheng72 <huangcheng20000702@gmail.com>
2023-03-03 02:11:41 +0800

    基于之前的简单空间配置器实现list部分。

---
+++ HCSTL/list.h
@@ -0,0 +1,214 @@
+//
+// Created by huangcheng on 2023/3/2.
+//
+
+#ifndef HCSTL_LIST_H
+#define HCSTL_LIST_H
+
+//STL的list是一个环形双向链表
+#include "type_traits.h"
+#include "allocator.h"
+#include "constructor.h"
+
+template <typename T>
+struct __list_node {
+    __list_node* prev;    //指向上一结点的指针
+    __list_node* next;    //指向下一结点的指针
+    T value;            //数据
+};
+
+template <typename T, typename Alloc = allocator<__list_node<T>> >
+class list {
+protected:
+    typedef __list_node<T> list_node;
+public:
+    typedef list_node* link_type;
+
+    typedef size_t size_type;   //这个不用说了，前面vector都知道了
+protected:
+    link_type node;     //只需一个结点即可表示整个环形双向链表
+    typedef Alloc data_allocator;   //表示空间配置器
+    link_type m_CreatNode(){    //创建一个结点，这个结点不带有意义的数据
+        link_type res = data_allocator::allocate(1);
+        if(hc_type_bool<typename _type_traits<T>::has_trivial_default_constructor>::value){
+            //是POD类型无需构造，直接填充数据0
+            res->value = 0;
+        } else {
+            //non-POD类型需要调用默认构造器
+            construct(&(res->value));
+        }
+        return res;
+    }
+    link_type m_CreatNode(const T& value){    //创建一个结点，这个结点的数据拷贝自value
+        link_type res = data_allocator::allocate(1);
+        if(hc_type_bool<typename _type_traits<T>::has_trivial_copy_constructor>::value){
+            //是POD类型无需构造，直接填充数据
+            res->value = value;
+        } else {
+            //non-POD类型需要调用拷贝构造器
+            construct(&(res->value),value);
+        }
+        return res;
+    }
+    void m_DestroyNode(link_type& n){    //销毁一个结点
+        if(!hc_type_bool<typename _type_traits<T>::has_trivial_destructor>::value){
+            //non-POD类型需要调用析构器
+            destroy(&(n->value));//析构数据;
+        }
+        destroy(n);//析构结点
+        data_allocator::deallocate(n);//回收空间
+    }
+
+public:
+    list() {
+        //创建一个空链表
+        node = m_CreatNode();
+        node->next = node;
+        node->prev = node;
+    }
+
+    ~list(){
+        clear();
+        m_DestroyNode(node);
+    }
+
+    link_type begin() {
+        return node->next;
+    }
+    link_type end() {
+        return node;    //node是不保存数据的，它相当于是最末尾的节点，为了符合前开后闭[first, last)，这个node起到last的作用
+    }
+    bool empty() const {
+        return node->next == node;
+    }
+    size_type size() const {
+        size_type count = 0;
+        for(link_type temp = node->next; temp != node; temp = temp->next, count++){}
+        return count;
+    }
+
+    link_type erase(link_type position) {     //消除某位置上元素
+        link_type temp = position->next;
+        position->prev->next = position->next;
+        position->next->prev = position->prev;//从链表中删除position这个结点
+        m_DestroyNode(position);
+        return temp;//现在在原来Position位置上的结点
+    }
+
+    link_type erase(link_type a, link_type b) {     //消除两个指针之间的元素，注意：a为起点，b为终点
+        //析构中间的元素，然后链接即可
+        link_type res = a->prev;
+        res->next = b;
+        b->prev = res;
+        for(link_type temp = a->next; temp != b; temp = temp->next){
+            //销毁结点之后temp->next的值将变化，所以应当先将指针后移，然后销毁prev，这样子就能规避这个问题
+            //temp == b的时候，正好销毁完了temp->prev，也就是b->prev，恰好完成任务
+            m_DestroyNode(temp->prev);
+        }
+        return b;//此时b在原来a的位置
+    }
+
+    void resize (size_type n){
+        if(n == 0){
+            //特殊情况
+            clear();
+        }
+        //多余删除，缺少就添加，默认构造
+        size_type count = size();
+        link_type _end = end();
+        if(n < count){
+            //多余要删除
+            for(size_type i = n; i > count; --i){
+                link_type temp = _end->prev;
+                _end->prev = temp->prev;
+                temp->prev->next = _end;//从链表中删除temp这个结点
+                m_DestroyNode(temp);
+            }
+        }else{
+            //缺少要添加
+            for(size_type i = count; i < n; ++i){
+                link_type temp = m_CreatNode();
+                temp->prev = _end->prev;
+                temp->next = _end;
+                temp->prev->next = temp;
+                temp->next->prev = temp;
+            }
+        }
+    }
+    void resize (size_type n, const T& val){
+        if(n == 0){
+            //特殊情况
+            clear();
+        }
+        //多余删除，缺少添加，拷贝构造
+        size_type count = size();
+        link_type _end = end();
+        if(n < count){
+            //多余要删除
+            for(size_type i = n; i > count; --i){
+                link_type temp = _end->prev;
+                _end->prev = temp->prev;
+                temp->prev->next = _end;//从链表中删除temp这个结点
+                m_DestroyNode(temp);
+            }
+        }else{
+            //缺少要添加
+            for(size_type i = count; i < n; ++i){
+                link_type temp = m_CreatNode(val);
+                temp->prev = _end->prev;
+                temp->next = _end;
+                temp->prev->next = temp;
+                temp->next->prev = temp;
+            }
+        }
+    }
+
+    T& front() {
+        return node->next->value;
+    }
+    T& back() {
+        return node->prev->value;
+    }
+
+    void push_back (const T& val) {
+        //拷贝构造
+        link_type temp = m_CreatNode(val);
+        temp->prev = end()->prev;
+        temp->next = end();
+        temp->prev->next = temp;
+        temp->next->prev = temp;
+    }
+    void push_front(const T& val) {
+        //拷贝构造
+        link_type temp = m_CreatNode(val);
+        temp->next = end()->next;
+        temp->next->prev = temp;
+        end()->next = temp;
+        temp->prev = end();
+    }
+    void pop_back(){
+        link_type temp = end()->prev;
+        end()->prev = temp->prev;
+        temp->prev->next = end();//从链表中删除temp这个结点
+        m_DestroyNode(temp);
+    }
+    void pop_front(){
+        link_type temp = end()->next;//要删除temp
+        end()->next = temp->next;
+        temp->next->prev = end();
+        m_DestroyNode(temp);
+    }
+
+    void clear(){
+        for(link_type temp = begin()->next; temp != end(); temp = temp->next){
+            //销毁结点之后temp->next的值将变化，所以应当先将指针后移，然后销毁prev，这样子就能规避这个问题
+            //temp == end()的时候，正好销毁完了temp->prev，也就是end()->prev，恰好完成任务
+            m_DestroyNode(temp->prev);
+        }
+        node->next = node;
+        node->prev = node;
+    }
+
+};
+
+#endif //HCSTL_LIST_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include "vector.h"
+#include "list.h"
 
 //用非POD类型来测试
 class myclass{
@@ -19,27 +19,28 @@ public:
 int main() {
     //以下是测试内容和应有结果
 
-    vector<myclass>& vec = *(new vector<myclass>(4));
+    list<myclass>& m_list = *(new list<myclass>());
 
     for(int i = 1; i < 10; i++){
         myclass& temp = *(new myclass(i*100));
-        vec.push_back(temp);
+        m_list.push_back(temp);
     }
 
-    std::cout << vec.size() << std::endl;                               //9
-    std::cout << vec.capacity() << std::endl;                           //16
-    std::cout << vec.front().getter() << std::endl;                     //100
-    std::cout << vec.back().getter() << std::endl;                      //900
+    std::cout << m_list.size() << std::endl;                               //9
+    std::cout << m_list.front().getter() << std::endl;                     //100
+    std::cout << m_list.back().getter() << std::endl;                      //900
 
-    vec.erase(vec.begin() + 2);                                 //删去的值是300
+    m_list.resize(11,*(new myclass(111)));
+    std::cout << m_list.size() << std::endl;                               //11
+    std::cout << m_list.back().getter() << std::endl;                      //111
+    m_list.pop_back();
+    m_list.pop_front();
+    std::cout << m_list.front().getter() << std::endl;                     //200
+    std::cout << m_list.back().getter() << std::endl;                      //111
 
-    std::cout << vec[2].getter() << std::endl;                          //400
-    std::cout << vec.size() << std::endl;                               //8
+    m_list.erase(m_list.begin(),m_list.end());
+    std::cout << m_list.size() << std::endl;                               //0
 
-    vec.erase(vec.begin() + 4, vec.end() - 2);                    //删去的值是600，700两个
-
-    std::cout << vec.size() << std::endl;                               //6
-    std::cout << vec[4].getter() << std::endl;                          //800
 
     return 0;
 }
